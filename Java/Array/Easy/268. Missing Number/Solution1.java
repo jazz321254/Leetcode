f9448cecd445ab3455f6cc9bc6183bb6d4c0348f
shawnyin <jazz321254@gmail.com>
2017-02-10 17:27:55 +0800

    Update Solution1.java

--- Java/Array/Easy/268. Missing Number/Solution1.java
+++ Java/Array/Easy/268. Missing Number/Solution1.java
@@ -10,8 +10,8 @@
  
 public int Solution1(int[] nums) {
     int xor = 0, i = 0;
-	for (i = 0; i < nums.length; i++) {
-		xor = xor ^ i ^ nums[i];
-	}
-	return xor ^ i;
+    for (i = 0; i < nums.length; i++) {
+        xor = xor ^ i ^ nums[i];
+    }
+    return xor ^ i;
 }
