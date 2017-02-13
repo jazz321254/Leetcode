0acb427b579c5a62cca83eec36198017e4c42fe7
shawnyin <jazz321254@gmail.com>
2017-02-13 16:06:38 +0800

    Update Solution.java

--- Java/Array/Easy/26. Remove Duplicates from Sorted Array/Solution.java
+++ Java/Array/Easy/26. Remove Duplicates from Sorted Array/Solution.java
@@ -2,7 +2,7 @@
  * <pre>
  * 因為不能使用其他資料結構
  * 所以用一個i紀錄有幾個不重複的值並存在原有的array裡
- * ps: 有點看不懂題目 感覺只要return length 但好像原nums也要不重複排列
+ * ps: 答案不只要return length是對的 原nums的length長度也要不重複排列
  * 時間複雜度 O(n) 空間複雜度 O(1)
  * </pre>
  *
