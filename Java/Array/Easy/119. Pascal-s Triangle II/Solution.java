d8923097d920f274a11deffc0d89616a2143e66a
shawnyin <jazz321254@gmail.com>
2017-02-15 12:09:32 +0800

    Create Solution.java

---
+++ Java/Array/Easy/119. Pascal-s Triangle II/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * <pre>
+ * 解題概念 處理完一列 在最前面加上1 在套公式算下一列的值
+ * 時間複雜度 O(n^2)
+ * </pre>
+ *
+ * @author Shawn
+ */
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+	    List<Integer> list = new ArrayList<Integer>();
+	    if (rowIndex < 0)
+		    return list;
+
+	    for (int i = 0; i < rowIndex + 1; i++) {
+		    list.add(0, 1);
+		    for (int j = 1; j < list.size() - 1; j++) {
+			    list.set(j, list.get(j) + list.get(j + 1));
+		    }
+	    }
+	    return list;
+    }
+}
